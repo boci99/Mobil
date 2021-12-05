13edc31d79041f7ba136dc2fc429c82c92e4500f
Botond <blascsak.botond99@gmail.com>
2021-12-05 14:53:01 +0100

    final

--- app/src/main/java/com/example/beadando/MainActivity.kt
+++ app/src/main/java/com/example/beadando/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var first = true
     var real_first = 0.0
     lateinit var stepsValue: TextView
-    //var running = false
-    //var sensorManager: SensorManager? = null
     lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +26,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-        //running = true
         Toast.makeText(this, "elindult", Toast.LENGTH_SHORT).show()
         var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
         //var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_LIGHT)
@@ -43,7 +40,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onPause() {
         super.onPause()
-        //running = false
         sensorManager?.unregisterListener(this)
     }
 
@@ -51,12 +47,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        //if (running) {
             if(first){
                 first = false
                 real_first = event.values[0].toDouble()
             }
             stepsValue.setText("" + (event.values[0]-real_first))
-        //}
     }
 }
